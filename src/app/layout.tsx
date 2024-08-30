bdbe05f714f7c31e8cd0aaaf55ba4b384a79698b
flan02 <chanivetdan@hotmail.com>
2024-08-30 11:53:11 -0300

    Adding new icons to main manu mobile page and added new opengraph image.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "2kceltics | Home",
   description: "2kceltics v2",
+  authors: {
+    name: "Dan Chanivet",
+    url: "https://danchanivet.tech",
+  },
+  publisher: "Vercel",
 };
 
 export default function RootLayout({
@@ -26,15 +31,16 @@ export default function RootLayout({
       <meta property="og:url" content="https://2kceltics.xyz" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="2kceltics | Home" />
-      <meta property="og:description" content="2kceltics v2" />
+      <meta property="og:description" content="2kceltics v2 created by Dan Chanivet" />
       <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/a13bd035-00fd-4ba3-a010-7c2c46260eba.jpg?token=-wLRIoq9gpRyzt23-Wi2L338yaGYQvsHhl7Yfufo1uA&height=630&width=1200&expires=33261025516" />
-
+      <meta property="og:image:alt" content="" />
+      <meta property="fb:app_id" content="" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="2kceltics.xyz" />
       <meta property="twitter:url" content="https://2kceltics.xyz" />
       <meta name="twitter:title" content="2kceltics | Home" />
-      <meta name="twitter:description" content="2kceltics v2" />
+      <meta name="twitter:description" content="2kceltics v2 created by Dan Chanivet" />
       <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/a13bd035-00fd-4ba3-a010-7c2c46260eba.jpg?token=-wLRIoq9gpRyzt23-Wi2L338yaGYQvsHhl7Yfufo1uA&height=630&width=1200&expires=33261025516">
       </meta>
       <body className={`${recursive.className}`}>
