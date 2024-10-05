de62282f5e2691b6bcd02311537db33810a3a1e8
Chanivet, Dan <chanivetdan@hotmail.com>
2024-10-05 19:47:50 -0300

    Update Playoffs.tsx
    Fixing an error in playoffs logo from desktopmenu component. Next  convert component as fully-responsive.

--- src/components/custom/Playoffs.tsx
+++ src/components/custom/Playoffs.tsx
@@ -8,7 +8,7 @@ const Playoffs = (props: Props) => {
   return (
     <div className='relative h-[230px] cursor-pointer border inline-block filter grayscale hover:grayscale-0 transition duration-300'>
       <a href="/playoffs" rel="noopener noreferrer">
-        <Image src={PlayoffsLogo} alt="playoffs" className='block lg:object-cover xl:object-contain px-8' sizes="(max-width: 768px) 100vw, 33vw" priority />
+        <Image src={PlayoffsLogo} alt="playoffs" className='h-full block lg:object-cover xl:object-contain px-8' sizes="(max-width: 768px) 100vw, 33vw" priority />
       </a>
 
     </div>
