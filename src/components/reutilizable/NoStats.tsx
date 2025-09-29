f610e05115626ad0ba2db6f7e9e5828dc4365343
flan02 <chanivetdan@hotmail.com>
2025-09-29 01:40:31 -0300

    Nostats.tsx component modified

--- src/components/reutilizable/NoStats.tsx
+++ src/components/reutilizable/NoStats.tsx
@@ -4,10 +4,12 @@ import MaxWidthWrapper from "./MaxWidthWrapper"
 type Props = {}
 
 const NoStats = (props: Props) => {
+
+  let msg = "No stats available" // Full stats are displayed from season 2024-25
   return (
     <MaxWidthWrapper className='min-h-[calc(70vh-150px)] mt-4 mb-12 pb-8 place-content-center text-celtics border'>
       <div className='flex justify-center items-center'>
-        <p className='text-xl lg:text-2xl text-center'>Full stats are displayed from season 2024-25.</p>
+        <p className='text-xl lg:text-2xl text-center'>{msg}</p>
       </div>
     </MaxWidthWrapper>
   )
