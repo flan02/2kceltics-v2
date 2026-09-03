8c4be6283774e1af63fd6e17eca43fff324865c0
flan02 <chanivetdan@hotmail.com>
2026-09-02 23:28:45 -0300

    update github secret vars

--- src/auth.ts
+++ src/auth.ts
@@ -1,47 +1,41 @@
-import NextAuth from 'next-auth'
-import GitHub from 'next-auth/providers/github'
-import { createUser, loggedAsAdmin } from './app/dashboard/actions'
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { createUser, loggedAsAdmin } from "./app/dashboard/actions";
 
-import { User } from 'types'
-
+import { User } from "types";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub(
-    {
+  providers: [
+    GitHub({
       clientId: process.env.AUTH_GITHUB_ID!,
       clientSecret: process.env.AUTH_GITHUB_SECRET!,
-      checks: ['none']
-    }
-  )],
+      checks: ["none"],
+    }),
+  ],
   callbacks: {
     async signIn({ user, account, profile }) {
-
       try {
-        const { name, email, image } = user as User
+        const { name, email, image } = user as User;
 
         //console.log({ user, account, profile });
         //console.log('Current email', email);
-        const userFound = await loggedAsAdmin(email) // We need to know if the user is an admin or not
+        const userFound = await loggedAsAdmin(email); // We need to know if the user is an admin or not
 
         if (!userFound) {
-          await createUser(name, email, image)
-          return false
-
+          await createUser(name, email, image);
+          return false;
         }
-        if (userFound.role == "ADMIN") return true
-        return false
+        if (userFound.role == "ADMIN") return true;
+        return false;
       } catch (error) {
-        console.error("We found the following error: ", error)
-        return false
-
+        console.error("We found the following error: ", error);
+        return false;
       }
-
-
-    }
+    },
   },
   pages: {
-    signIn: '/dashboard',
-    signOut: '/',
-    error: '/'
-  }
-})
+    signIn: "/dashboard",
+    signOut: "/",
+    error: "/",
+  },
+});
