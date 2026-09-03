53a58a135a516484cc5fba6bb023eafa3aea9467
flan02 <chanivetdan@hotmail.com>
2026-09-02 23:57:52 -0300

    update auth.js file

--- src/auth.ts
+++ src/auth.ts
@@ -10,7 +10,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientId: process.env.AUTH_GITHUB_ID!,
       clientSecret: process.env.AUTH_GITHUB_SECRET!,
       checks: ["none"],
-      issuer: "https://github.com",
+      issuer: "https://github.com/login/oauth",
     }),
   ],
   callbacks: {
